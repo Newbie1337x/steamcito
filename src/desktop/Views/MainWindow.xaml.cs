e9a28fc4e43539050b79326115ae3250dbc145cf
Tomas cabrera <tomascabrera918@gmail.com>
2026-03-17 13:21:26 -0300

    Revert "Merge branch 'main' of https://github.com/Newbie1337x/steamcito"
    This reverts commit 90d00eb4274abe56d3743a55ec216be8d6fb3ebc, reversing
    changes made to 6210e050226ebfa8d48cd5778b5c362aab144a72.

--- src/desktop/Views/MainWindow.xaml.cs
+++ src/desktop/Views/MainWindow.xaml.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
 using steamcito.ViewModels;
 namespace steamcito.Views
 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
         MainWindowModel mainWindowModel;
@@ -16,81 +17,7 @@ namespace steamcito.Views
             LibraryViewControl.Visibility = Visibility.Visible;
             SettingsViewControl.Visibility = Visibility.Collapsed;
             DataContext = mainWindowModel;
-            StateChanged += MainWindow_StateChanged;
-            SourceInitialized += MainWindow_SourceInitialized;
-            StateChanged += MainWindow_StateChanged;
         }
-        
-        private void MainWindow_SourceInitialized(object sender, EventArgs e)
-        {
-            IntPtr handle = new System.Windows.Interop.WindowInteropHelper(this).Handle;
-            System.Windows.Interop.HwndSource.FromHwnd(handle)?.AddHook((WindowProc));
-        }
-        
-        private void MainWindow_StateChanged(object sender, EventArgs e)
-        {
-            if (WindowState == WindowState.Maximized)
-            {
-                MainBorder.Margin = new Thickness(0); 
-            }
-            else
-            {
-                MainBorder.Margin = new Thickness(0);
-            }
-        }
-        
-        private IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        {
-            if (msg == 0x0024)
-            {
-                WmGetMinMaxInfo(hwnd, lParam);
-                handled = true;
-            }
-            return IntPtr.Zero;
-        }
-        
-        [DllImport("user32.dll")]
-        private static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
-
-        private void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam)
-        {
-            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
-            int MONITOR_DEFAULTTONEAREST = 0x00000002;
-            IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
-
-            if (monitor != IntPtr.Zero)
-            {
-                MONITORINFO monitorInfo = new MONITORINFO();
-                monitorInfo.cbSize = Marshal.SizeOf(typeof(MONITORINFO));
-                GetMonitorInfo(monitor, ref monitorInfo);
-
-                // El "WorkArea" es el espacio del monitor MENOS la barra de tareas
-                RECT rcWorkArea = monitorInfo.rcWork;
-                RECT rcMonitorArea = monitorInfo.rcMonitor;
-
-                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
-                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
-                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
-                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
-            }
-
-            Marshal.StructureToPtr(mmi, lParam, true);
-        }
-        
-        [StructLayout(LayoutKind.Sequential)]
-        public struct POINT { public int x; public int y; }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct MINMAXINFO { public POINT ptReserved; public POINT ptMaxSize; public POINT ptMaxPosition; public POINT ptMinTrackSize; public POINT ptMaxTrackSize; }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT { public int left; public int top; public int right; public int bottom; }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct MONITORINFO { public int cbSize; public RECT rcMonitor; public RECT rcWork; public int dwFlags; }
 
 
         private void AddProduct_Click(object sender, RoutedEventArgs e)
@@ -133,14 +60,13 @@ namespace steamcito.Views
         {
             if (e.ClickCount == 2)
             {
-                Maximize_Click(sender, e);
+                WindowState = WindowState == WindowState.Maximized
+                    ? WindowState.Normal
+                    : WindowState.Maximized;
             }
             else
             {
-                if (WindowState == WindowState.Normal)
-                {
-                    DragMove();
-                }
+                DragMove();
             }
         }
     }
