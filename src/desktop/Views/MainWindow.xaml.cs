fb8f3ae3553c96ded4a881cbbf6f9fe45ef548a8
lucasmdv <lucasmdv11@gmail.com>
2026-03-26 14:34:00 -0300

    refactor: deleted unnecesary code

--- src/desktop/Views/MainWindow.xaml.cs
+++ src/desktop/Views/MainWindow.xaml.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Interop;
 using Microsoft.Extensions.DependencyInjection;
 using steamcito.ViewModels;
+
 namespace steamcito.Views
 {
     public partial class MainWindow : Window
@@ -15,13 +14,13 @@ namespace steamcito.Views
         public MainWindow()
         {
             InitializeComponent();
+            
             _mainWindowModel = App.ServiceProvider?.GetRequiredService<MainWindowModel>() 
                               ?? throw new InvalidOperationException("ServiceProvider is not initialized");
+                              
             LibraryViewControl.Visibility = Visibility.Visible;
             SettingsViewControl.Visibility = Visibility.Collapsed;
             DataContext = _mainWindowModel;
-            
-            SourceInitialized += MainWindow_SourceInitialized;
             
             CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, (s, e) => SystemCommands.CloseWindow(this)));
             CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, (s, e) => SystemCommands.MinimizeWindow(this)));
@@ -33,66 +32,6 @@ namespace steamcito.Views
                     SystemCommands.MaximizeWindow(this);
             }));
         }
-        
-        private void MainWindow_SourceInitialized(object sender, EventArgs e)
-        {
-            IntPtr handle = new WindowInteropHelper(this).Handle;
-            HwndSource.FromHwnd(handle)?.AddHook((WindowProc));
-        }
-        
-        private IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        {
-            if (msg == 0x0024)
-            {
-                WmGetMinMaxInfo(hwnd, lParam);
-                handled = true;
-            }
-            return IntPtr.Zero;
-        }
-        
-        [DllImport("user32.dll")]
-        private static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
-
-        private void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam)
-        {
-            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
-            int MONITOR_DEFAULTTONEAREST = 0x00000002;
-            IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
-
-            if (monitor != IntPtr.Zero)
-            {
-                MONITORINFO monitorInfo = new MONITORINFO();
-                monitorInfo.cbSize = Marshal.SizeOf(typeof(MONITORINFO));
-                GetMonitorInfo(monitor, ref monitorInfo);
-
-                // El "WorkArea" es el espacio del monitor MENOS la barra de tareas
-                RECT rcWorkArea = monitorInfo.rcWork;
-                RECT rcMonitorArea = monitorInfo.rcMonitor;
-
-                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
-                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
-                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
-                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
-            }
-
-            Marshal.StructureToPtr(mmi, lParam, true);
-        }
-        
-        [StructLayout(LayoutKind.Sequential)]
-        public struct POINT { public int x; public int y; }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct MINMAXINFO { public POINT ptReserved; public POINT ptMaxSize; public POINT ptMaxPosition; public POINT ptMinTrackSize; public POINT ptMaxTrackSize; }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT { public int left; public int top; public int right; public int bottom; }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct MONITORINFO { public int cbSize; public RECT rcMonitor; public RECT rcWork; public int dwFlags; }
-
 
         private void AddProduct_Click(object sender, RoutedEventArgs e)
         {
